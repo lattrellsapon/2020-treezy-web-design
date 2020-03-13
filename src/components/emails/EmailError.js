9562516e489c43653567c7f09a7ab4c195f14907
lattrellsapon <lattrellsapon@gmail.com>
2020-03-14 12:49:01 +1300

    Fixed the email error message

--- src/components/emails/EmailError.js
+++ src/components/emails/EmailError.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class EmailError extends Component {
   render() {
     return (
-      <div>
+      <div className='container'>
         <h1>Sorry, there was an error. Please try again later.</h1>
       </div>
     );
